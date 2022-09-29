3c21f7d417fe02b3eefef27e5721696dd44dc9b8
braguinnha <110104640+braguinnha@users.noreply.github.com>
2022-09-29 12:47:20 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,14 @@
+let tabuada = 2;
 
+function Escreva(){
+  document.write(tabuada + " x 1 = " + (tabuada*1));
+  document.write(tabuada + " x 2 = " + (tabuada*2));
+  document.write(tabuada + " x 3 = " + (tabuada*2));
+  document.write(tabuada + " x 4 = " + (tabuada*2));
+  document.write(tabuada + " x 5 = " + (tabuada*2));
+  document.write(tabuada + " x 6 = " + (tabuada*2));
+  document.write(tabuada + " x 7 = " + (tabuada*2));
+  document.write(tabuada + " x 8 = " + (tabuada*2));
+  document.write(tabuada + " x 9 = " + (tabuada*2));
+  document.write(tabuada + " x 10 = " + (tabuada*2));
+}
